fcba41f5736b7f274926f6d5d2157ef1a9fda637
Ash Lewis <ashlewis@github.com>
2015-11-05 17:00:31 +0000

    add mailcatcher config

--- puphpet/files/startup-always/start-mailcatcher.sh
+++ puphpet/files/startup-always/start-mailcatcher.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #
 mailcatcher --http-ip=$mailcatcher::params::http_ip
+
+if ! grep -R "mailcatcher_custom" "/etc/postfix/main.cf"; then
+    echo 'Adding custom mailcatcher config'
+    printf "\n# mailcatcher_custom\nrelayhost = 127.0.0.1:1025\n" >> /etc/postfix/main.cf
+    /etc/init.d/postfix restart
+fi
