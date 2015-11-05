f9186304d3b995ffc542ef82b6b9900d571487d9
Ash Lewis <ashlewis@github.com>
2015-11-05 12:36:03 +0000

    remove broken/slow packages from config file, move drush update to always as puphpet version overrides on provision, fix custom zsh config

---
+++ puphpet/files/exec-always/update-drush.sh
@@ -0,0 +1,4 @@
+# Install a specific version of Drush, e.g. Drush 7.1.0
+composer global require drush/drush:7.1.0
+rm -rf /usr/share/drush
+drush cc drush
--- puphpet/files/exec-once/oh-my-zsh-install.sh
+++ puphpet/files/exec-once/oh-my-zsh-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #
+
+echo 'Installing oh-my-zsh'
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+
+if ! grep -R "zshrc_custom" "$HOME/.zshrc"; then
+    echo 'Adding custom oh-my-zsh config'
+    printf "\n# zshrc_custom\nsource .zshrc_custom\n" >> $HOME/.zshrc
+fi
