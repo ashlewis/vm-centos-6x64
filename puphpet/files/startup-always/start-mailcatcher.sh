9fa58444c93c67bfccd6aa19f204a817ff67fe45
Ash Lewis <ashlewis@github.com>
2015-11-17 17:27:57 +0000

    fix mailcatcher config TODO: fix properly :-)

--- puphpet/files/startup-always/start-mailcatcher.sh
+++ puphpet/files/startup-always/start-mailcatcher.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-mailcatcher --http-ip=$mailcatcher::params::http_ip
+#
+
+echo "Starting mailcatcher..."
+#mailcatcher --http-ip=$mailcatcher::params::http_ip
+mailcatcher --http-ip=192.168.60.101
 
 if ! grep -R "mailcatcher_custom" "/etc/postfix/main.cf"; then
     echo 'Adding custom mailcatcher config'
